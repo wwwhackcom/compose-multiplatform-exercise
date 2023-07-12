dfca7bc5b1e365130ec6ce72cc3bb0cff035220a
Nick Wang <wwwhackcom@qq.com>
2023-07-12 22:03:04 +1200

    Crafted new api for the demo app
    APIs written in nodejs.
    Now there are 3 restful apis: login, register, user.

--- shared/src/commonMain/kotlin/net.wwwhackcom/network/Api.kt
+++ shared/src/commonMain/kotlin/net.wwwhackcom/network/Api.kt
@@ -3,10 +3,17 @@ package net.wwwhackcom.network
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.client.request.get
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.header
+import io.ktor.client.request.post
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.URLProtocol
+import io.ktor.http.contentType
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
-import net.wwwhackcom.RocketLaunch
+import net.wwwhackcom.Credential
+import net.wwwhackcom.User
 
 /**
  * @author nickwang
@@ -14,10 +21,11 @@ import net.wwwhackcom.RocketLaunch
  */
 
 interface Api {
-    suspend fun getAllLaunches(): List<RocketLaunch>
+    suspend fun login(credential: Credential): Dto<User>
 }
 
-class ApiImpl: Api {
+
+class ApiImpl : Api {
 
     private val httpClient = HttpClient {
         install(ContentNegotiation) {
@@ -26,9 +34,28 @@ class ApiImpl: Api {
                 useAlternativeNames = false
             })
         }
+        defaultRequest {
+            url {
+                protocol = URLProtocol.HTTPS
+                host = baseUrl
+            }
+            header(X_API_CONSUMER, API_KEY)
+            contentType(ContentType.Application.Json)
+        }
     }
 
-    override suspend fun getAllLaunches(): List<RocketLaunch> {
-        return httpClient.get("https://api.spacexdata.com/v5/launches").body()
+    override suspend fun login(credential: Credential): Dto<User> {
+        return httpClient.post("/auth/login") {
+            setBody(credential)
+        }.body()
     }
+
+    companion object {
+
+        private const val baseUrl = "api.wwwhackcom.net"
+        private const val X_API_CONSUMER = "api-key"
+        private const val API_KEY = "3e8288cf-e626-4557-81a8-517e10dec6f1"
+
+    }
+
 }
